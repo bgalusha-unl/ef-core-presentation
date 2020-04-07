5c0400e01c6abb3cb04d920dc03218e8af9351cd
Ben Galusha <ben.galusha@huskers.unl.edu>
2020-04-07 16:48:56 -0500

    add database insert example

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -8,7 +8,7 @@ namespace Presentation
     class Program
     {
         static int USERS = 1;
-        static int POSTS_PER_USER = 5;
+        static int POSTS_PER_USER = 0;
 
         static void Main( string[] args )
         {
@@ -20,10 +20,23 @@ namespace Presentation
                 Filler.Populate( USERS, POSTS_PER_USER, db );
 
                 User testUser = db.Users
-                    .Include(u => u.Posts)
+                    .Include( u => u.Posts )
                     .First();
+
+                Post newPost = new Post
+                {
+                    PostId = 0, // so EF Core can tell that this object is new
+                    User = testUser,
+                    Likes = 99,
+                    Message = "Now is the time for all good men to come to the aid of their country."
+                };
+
+                testUser.Posts.Add( newPost );
+                db.Users.Update( testUser );
+                db.SaveChanges();
+
                 Console.WriteLine( "\n{0}'s Posts:\n", testUser.Email );
-                foreach ( Post post in testUser.Posts )
+                foreach ( Post post in db.Posts.Where( p => p.User == testUser ) )
                 {
                     Console.WriteLine( post );
                 }
